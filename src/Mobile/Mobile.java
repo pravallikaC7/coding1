792f1b5470d879e7d74d8441326bb3b55aac58b4
pravallikaC7 <pravallikachilukuri7@gmail.com>
2021-04-11 13:35:12 +0530

    Firstcreation
    Change-Id: Icb26e1013d29935fa87421922a2c8fa580bea493

---
+++ src/Mobile/Mobile.java
@@ -0,0 +1,32 @@
+package Mobile;
+
+import java.util.Scanner;
+
+public class Mobile {
+	
+	int[][] mobileArray = {{'a','b','c'}, {'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r'},{'s','t','u','v'},{'w','x','y','z'}};
+	public int mobi(int x, int y)
+	{
+		return mobileArray[x-1][y-1];
+	}
+
+	public static void main(String[] args) {
+		int size;
+		Scanner scanObj=new Scanner(System.in);
+		size = scanObj.nextInt();
+		int[] input = new int[size];
+		for (int i=0; i < size; i++)
+			input[i]=scanObj.nextInt();
+		for (int i=0; i<size; i+=2)
+		{
+			int a=input[i];
+			int b= input[i+1];
+			Mobile mobileObj = new Mobile();
+			char req= (char)(mobileObj.mobi(a,b));
+			System.out.println(req+" ");
+		}
+		
+
+	}
+
+}
